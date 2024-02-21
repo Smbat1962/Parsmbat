ad551f419dcd0a01340818eefdb773ec1c0dafd2
milaaspram <parsadanyansmbat@gmail.com>
2024-02-21 13:23:27 +0400

    Graph of forced oscillations in the presence of friction

---
+++ graph_forced_oscillations.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+
+w = 20
+v = 0.3
+f = 0.4
+m = 1 
+c = 5
+
+
+def z_derivatives(x, t):
+    return [x[1], (-2*v*x[1] - x[0]*w**2  + (f/m)*np.cos(c*t))]
+
+time = np.arange(0, 10, 1e-3)
+position, velocity = odeint(z_derivatives, [2e-3, 0], time).T   
+plt.plot(time, position*100, 'orange', linewidth = 2)
+plt.xlabel('time (s)')
+plt.ylabel('position (cm)')
+plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+plt.grid(True)
+plt.show()
